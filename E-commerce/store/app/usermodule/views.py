a1f80e403d5ace315b823e24ae5a5c6e470040b2
Abdullah <AbooD1422AbooD@gmail.com>
2024-03-29 03:13:36 +0300

    added a product page that showes the product images and added a static folder

--- E-commerce/store/app/usermodule/views.py
+++ E-commerce/store/app/usermodule/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'usermodule/home.html')
+    return render(request, 'usermodule/home.html', {'product': __getproduct()})
 
 def login(request):
     return render(request, 'usermodule/login.html')
+
+def product(request, id):
+    products = __getproduct()
+    product = None
+    for p in products:
+        if p['id'] == id:
+            product = p
+    context = {'product':product}    
+    return render(request, 'usermodule/product.html', context)
+
+def __getproduct():
+    products = [
+        {'id':1, 'pname':'Iphone 15', 'company':'Apple'},
+        {'id':2, 'pname':'Galaxy S24 ultra', 'company':'Samsong'},
+        {'id':3, 'pname':'Ipad Air 5', 'company':'Apple'},
+    ]
+    return products
